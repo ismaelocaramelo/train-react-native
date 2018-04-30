091f211f62ab03918585da6e04c2b1898e47d224
Ismael Bakkali <ismaelbacha@hotmail.com>
2018-04-30 09:49:16 +0200

    loading images component

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { AppRegistry } from 'react-native';
 import MainApp from './src/MainApp';
+import LoadingImage from './src/LoadingImage';
 
-AppRegistry.registerComponent('myApp', () => MainApp);
+//AppRegistry.registerComponent('myApp', () => MainApp);
+AppRegistry.registerComponent('myApp', () => LoadingImage);
