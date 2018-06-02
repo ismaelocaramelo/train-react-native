f634e7c784c4a1bf749dd8f9774e2920cb7a18d5
Ismael Bakkali <ismaelbacha@hotmail.com>
2018-06-02 13:16:46 +0200

    icos view added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import MainApp from './src/MainApp';
 import MainAppNav from './src/MainAppNav';
 import MainAppButton from './src/MainAppButton';
 import MainAppPost from './src/MainAppPost';
+import MainAppIcon from './src/MainAppIcon';
 
 import LoadingImage from './src/LoadingImage';
 import Toggle from './src/Toggle';
@@ -12,4 +13,4 @@ import Profile from './src/Profile';
 import CustomFontIos from './src/CustomFontIos';
 
 //AppRegistry.registerComponent('myApp', () => MainApp);
-AppRegistry.registerComponent('myApp', () => CustomFontIos);
+AppRegistry.registerComponent('myApp', () => MainAppIcon);
