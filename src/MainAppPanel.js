1c6db32c493a653fc8829aa3a966a87d0502542f
Ismael Bakkali <ismaelbacha@hotmail.com>
2018-06-09 19:34:44 +0200

    with title

--- src/MainAppPanel.js
+++ src/MainAppPanel.js
@@ -6,15 +6,38 @@ const MainApp = () => (
   <View style={styles.main}>
     <Text style={styles.toolbar}>Animated containers</Text>
     <View style={styles.content}>
-      <Panel style={styles.panel}>
-        <Text>Lorem ipsum dolor sit amet</Text>
+      <Panel title="This is the first container" style={styles.panel}>
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Text>
       </Panel>
-      <Panel style={styles.panel}>
-        <Text>Doius sit amet</Text>
+      <Panel title="Second panel in the view" style={styles.panel}>
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Duis aute
+          irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+          sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </Text>
       </Panel>
-      <Panel expanded style={styles.panel}>
-        <Text>Lorem ipsum dolor sit amet</Text>
-        <Text>Lorem ipsum ...</Text>
+      <Panel expanded title="One more to test" style={styles.panel}>
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </Text>
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </Text>
       </Panel>
     </View>
   </View>
@@ -27,13 +50,14 @@ const styles = StyleSheet.create({
   toolbar: {
     backgroundColor: '#3498db',
     color: '#fff',
+    fontSize: 22,
     padding: 20,
     textAlign: 'center'
   },
   content: {
-    flex: 1,
     padding: 10,
-    backgroundColor: '#ecf0f1'
+    backgroundColor: '#ecf0f1',
+    flex: 1
   },
   panel: {
     marginBottom: 10
